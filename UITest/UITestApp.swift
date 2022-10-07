c10881caef9e28e6a18be4fda2ad7370a7820c11
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2022-10-07 03:36:32 -0400

    Initial Commit

---
+++ UITest/UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestApp.swift
+//  UITest
+//
+//  Created by Chowdhury Md Rajib  Sarwar on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
